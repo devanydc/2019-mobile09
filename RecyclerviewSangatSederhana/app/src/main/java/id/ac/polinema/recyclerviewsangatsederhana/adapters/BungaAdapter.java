7e9d0e9a1613520f32f700f5d72f45e0c0951104
devanydc <devany1212.dc@gmail.com>
2019-10-30 06:21:49 +0700

    Tugas 1 dan 2 has done

--- RecyclerviewSangatSederhana/app/src/main/java/id/ac/polinema/recyclerviewsangatsederhana/adapters/BungaAdapter.java
+++ RecyclerviewSangatSederhana/app/src/main/java/id/ac/polinema/recyclerviewsangatsederhana/adapters/BungaAdapter.java
@@ -1,18 +1,21 @@
 package id.ac.polinema.recyclerviewsangatsederhana.adapters;
+
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-import org.w3c.dom.Text;
+
 import java.util.List;
 
 import id.ac.polinema.recyclerviewsangatsederhana.R;
+import id.ac.polinema.recyclerviewsangatsederhana.models.bunga;
 
-public class BungaAdapter {
+public class BungaAdapter extends RecyclerView.Adapter<BungaAdapter.MyView>{
     List<bunga> bungaList;
 
     public BungaAdapter(List<bunga> bungaList){
@@ -33,13 +36,25 @@ public class BungaAdapter {
     @Override
     public void onBindViewHolder(@NonNull BungaAdapter.MyView holder, int position){
         bunga bunga=bungaList.get(position);
-        holder.namaBunga.setText(bunga.getBunga());
-        holder.detail.setText(bunga.getNambahBuah());
-        holder.gambar.setImageResource(bunga.getBuah());
+        holder.nama.setText(bunga.getNamaBunga());
+        holder.detail.setText(bunga.getDetailBunga());
+        holder.gambar.setImageResource(bunga.getBunga());
     }
     @Override
     public int getItemCount(){
-        return
+        return (bungaList != null ? bungaList.size() : 0);
+    }
+
+    public class MyView extends RecyclerView.ViewHolder{
+        public TextView nama;
+        public TextView detail;
+        public ImageView gambar;
+        public MyView(@NonNull View itemView) {
+            super(itemView);
+            nama =itemView.findViewById(R.id.namaBunga);
+            detail=itemView.findViewById(R.id.detailBunga);
+            gambar=itemView.findViewById(R.id.gambarBunga);
+        }
     }
 
 }
